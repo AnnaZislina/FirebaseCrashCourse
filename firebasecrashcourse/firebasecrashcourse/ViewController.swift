039c973d38246315483a80a127e88aebd4da28ef
Anna Zislina <annzislina@gmail.com>
2020-03-28 13:41:35 +0300

    Reading, updating and deleting data methods

--- firebasecrashcourse/firebasecrashcourse/ViewController.swift
+++ firebasecrashcourse/firebasecrashcourse/ViewController.swift
@@ -17,11 +17,32 @@ class ViewController: UIViewController {
         
         let ref = Database.database().reference()
         
-        //specify the key path to the location to insert the data to:
+//MARK: Creating data
+        
+        //Specify the key path to the location to insert the data to:
         //ref.child("someid-name").setValue("Anna")
         
-        //auto-generated random unique key:
-        ref.childByAutoId().setValue(["name":"Tom", "role":"Admin", "age":"30"])
+        //Auto-generated random unique key:
+        //ref.childByAutoId().setValue(["name":"Tom", "role":"Admin", "age":"30"])
+        
+//MARK: Reading the data
+        
+//        ref.child("someid/name").observeSingleEvent(of: .value) { (snapshot) in
+//            let employeeData = snapshot.value as? [String:Any]
+//        }
+        
+//MARK: Updating data
+        
+        //ref.child("someid/name").setValue("John")
+        
+        //Update several values at once:
+//        let updates = ["someid/name":"John", "-M3Vdj7D2hcoMNUw9T0e/name":"David"]
+//        ref.updateChildValues(updates)
+        
+        
+//MARK: Deleting data
+        
+        ref.child("someid").removeValue()
     }
 
 
