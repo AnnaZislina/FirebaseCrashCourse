02d81595d978fcbd4d17f7fcb93f584dc2d3ba5e
Anna Zislina <annzislina@gmail.com>
2020-03-28 13:22:42 +0300

    Create a Database reference

--- firebasecrashcourse/firebasecrashcourse/ViewController.swift
+++ firebasecrashcourse/firebasecrashcourse/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let ref = Database.database().reference()
+        
+        //specify the key path to the location to insert the data to:
+        //ref.child("someid-name").setValue("Anna")
+        
+        //auto-generated random unique key:
+        ref.childByAutoId().setValue(["name":"Tom", "role":"Admin", "age":"30"])
     }
 
 
